e41636b6b318997fec71dc408525a5eaf405ccef
almaho <126069668+almaho@users.noreply.github.com>
2024-11-28 16:52:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ import "dotenv/config";
 
 const app = express()
 UserRoutes(app);
-app.use(cors({ credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
-})
-);
+app.use(cors({
+    origin: 'https://6748e4e850b97d906c52f53c--splendorous-donut-75047d.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
 
 
       const sessionOptions = {
